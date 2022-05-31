1991c8e9d07a293a76ed49d4c4769a50a2fd7800
Cleiton Trindade <93351789+CleitonTrindadeDev@users.noreply.github.com>
2022-05-30 21:38:35 -0300

    atualizando curriculo

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ var hamburguer = document.querySelector(".hamburguer");
 hamburguer.addEventListener("click", function(){
   document.querySelector(".container").classList.toggle("show-menu");
 });
+
